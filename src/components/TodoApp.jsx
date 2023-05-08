3f71f3e1a62e30f01fe2cb01a30000da602d3841
Jawad <Jawad.Nazari@outlook.com>
2023-05-08 18:47:14 +0500

    initial commit

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header';
+
+// other imports here
+import TodosLogic from '@/components/TodosLogic';
+
+const TodoApp = () => {
+  return (
+    <>
+      <Header />
+      <TodosLogic />
+    </>
+  );
+};
+export default TodoApp;
+
